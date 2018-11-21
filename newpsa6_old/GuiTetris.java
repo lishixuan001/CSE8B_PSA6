f68f281ad30db39d0a5bb4be17f3baad6542dbea
Shixuan Li <lishixuan001@berkeley.edu>
2018-11-20 20:25:08 -0800

    asd

--- newpsa6_old/GuiTetris.java
+++ newpsa6_old/GuiTetris.java
@@ -128,13 +128,15 @@ public class GuiTetris extends Application {
         }
         for (int i = 0; i < rectNext.length; i++) {
             for (int j = 0; j < rectNext[0].length; j++) {
-                if (i < nextPiece.tiles.length && j < nextPiece.tiles[0].length) {
-                    if (nextPiece.tiles[i][j] == 1) {
-                        this.rectNext[i][j] = new Rectangle(size, size, color);
-                        this.pane.add(this.rectNext[i][j], j + 6, i + 2);
-                    }
-                } else {
-                    this.rectNext[i][j] = new Rectangle(size, size, Color.WHITE);
+                this.rectNext[i][j] = new Rectangle(size, size, Color.RED);
+                this.pane.add(this.rectNext[i][j], j + 6, i + 2);
+            }
+        }
+
+        for (int i = 0; i < nextPiece.tiles.length; i++) {
+            for (int j = 0; j < nextPiece.tiles[0].length; j++) {
+                if (nextPiece.tiles[i][j] == 1) {
+                    this.rectNext[i][j] = new Rectangle(size, size, Color.BLACK);
                     this.pane.add(this.rectNext[i][j], j + 6, i + 2);
                 }
             }
